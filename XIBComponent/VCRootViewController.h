82bd250933a2172758d0a5359d49542b469cad4e
廖宝裕 <1576660896@qq.com>
2019-05-30 10:09:59 +0800

    使用XIB实现分栏控制器界面设计

---
+++ XIBComponent/VCRootViewController.h
@@ -0,0 +1,13 @@
+//
+//  VCRootViewController.h
+//  XIBComponent
+//
+//  Created by Civet on 2019/5/22.
+//  Copyright © 2019年 PandaTest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VCRootViewController : UIViewController
+
+@end
